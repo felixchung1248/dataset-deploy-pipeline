13ecd7d051f5981125e729c6b4f3a2551015df2a
Felix <felix.chung@felixchung.net>
2024-03-22 18:23:33 +0800

    Add data catalog integration

--- Jenkinsfile
+++ Jenkinsfile
@@ -4,11 +4,12 @@ pipeline {
     parameters {
         string(name: 'DATASET_NAME', description: 'Dataset name')
         string(name: 'TICKET_ID', description: 'Ticket ID')
-        string(name: 'ARTICLE_ID', description: 'Article ID')
     }
 
     environment {
         DATAHUB_TOKEN = credentials('datahub-token')
+        ZAMMAD_USR = credentials('zammad-usr')
+        ZAMMAD_PW = credentials('zammad-pw')
     }
 
     stages {
@@ -20,9 +21,6 @@ pipeline {
                     }
                     if (!params.TICKET_ID) {
                         error "The TICKET_ID parameter is missing. Please provide the ticket ID."
-                    }
-                    if (!params.ARTICLE_ID) {
-                        error "The ARTICLE_ID parameter is missing. Please provide the article ID."
                     }
                 }
             }
@@ -48,24 +46,24 @@ pipeline {
             }
         }
 		
-	// 	stage('Deploy data catalog dataset') {
-    //        steps {
-    //            script {					
+		stage('Deploy data catalog dataset') {
+           steps {
+               script {					
 					
-    //                // Run the Python script within the Docker container
-    //                docker.withRegistry("https://${env.DOCKER_REGISTRY}", env.DOCKER_REGISTRY_CREDENTIALS_ID) {
-    //                    // Create a Docker image object
-    //                    def pythonImage = docker.image("${env.DOCKER_REGISTRY}/${env.GOLDEN_PROJECT_NAME}/${env.GOLDEN_DOCKER_IMAGE}:${env.GOLDEN_DOCKER_TAG}")
-    //                    // Run the container with the script mounted and execute the Python script
-    //                    pythonImage.inside("-v ${env.WORKSPACE}:/app/workspace") {
-    //                        sh """
-    //                            python3 /app/workspace/data-catalog-deploy.py batch_key=${env.BATCH_KEY} token=${env.DATAHUB_TOKEN}
-	// 						   """
-    //                    }
-    //                }
-    //            }
-    //        }
-    //    }
+                   // Run the Python script within the Docker container
+                   docker.withRegistry("https://${env.DOCKER_REGISTRY}", env.DOCKER_REGISTRY_CREDENTIALS_ID) {
+                       // Create a Docker image object
+                       def pythonImage = docker.image("${env.DOCKER_REGISTRY}/${env.GOLDEN_PROJECT_NAME}/${env.GOLDEN_DOCKER_IMAGE}:${env.GOLDEN_DOCKER_TAG}")
+                       // Run the container with the script mounted and execute the Python script
+                       pythonImage.inside("-v ${env.WORKSPACE}:/app/workspace") {
+                           sh """
+                               python3 /app/workspace/data-catalog-deploy.py ticket_id=${env.TICKET_ID} token=${env.DATAHUB_TOKEN} zammad_usr=${env.ZAMMAD_USR} zammad_pw=${env.ZAMMAD_PW} zammad_url=${env.ZAMMAD_URL} dataset=${env.DATASET_NAME}
+							   """
+                       }
+                   }
+               }
+           }
+       }
     }
 
    post {
---
+++ data-catalog-deploy.py
@@ -0,0 +1,112 @@
+# Inlined from /metadata-ingestion/examples/library/dataset_schema.py
+# Imports for urn construction utility methods
+from datahub.emitter.mce_builder import make_data_platform_urn, make_dataset_urn
+from datahub.emitter.mcp import MetadataChangeProposalWrapper
+from datahub.emitter.rest_emitter import DatahubRestEmitter
+
+import requests
+import sys
+from requests.auth import HTTPBasicAuth
+import csv
+from io import StringIO
+
+
+# Imports for metadata model classes
+from datahub.metadata.schema_classes import *
+
+
+def case1():
+    return StringTypeClass()
+
+def case2():
+    return BooleanTypeClass()
+
+def case3():
+    return NumberTypeClass()
+
+def case4():
+    return DateTypeClass()
+
+def case5():
+    return TimeTypeClass()
+
+# Function to parse key-value pairs from command line arguments
+def parse_key_value_pairs(argv):
+    params = {}
+    for arg in argv[1:]:  # Skip the script name
+        key, value = arg.split('=', 1)
+        params[key] = value
+    return params
+
+parameters = parse_key_value_pairs(sys.argv)
+# batchKey = parameters['batch_key']
+ticketId = parameters['ticket_id']
+token = parameters['token']
+zammad_usr = parameters['zammad_usr']
+zammad_pw = parameters['zammad_pw']
+zammad_url = parameters['zammad_url']
+dataset = parameters['dataset']
+
+response = requests.get(
+            f"{zammad_url}/api/v1/ticket_articles/by_ticket/{ticketId}",
+            auth=HTTPBasicAuth(zammad_usr, zammad_pw)
+        )
+
+data = response.json()
+articleId = data[0]['id']
+attachments = data[0]['attachments']
+attachmentId = attachments[0]['id']
+
+response = requests.get(
+            f"{zammad_url}/api/v1/ticket_attachment/{ticketId}/{articleId}/{attachmentId}",
+            auth=HTTPBasicAuth(zammad_usr, zammad_pw)
+        )
+
+csv_content = response.text
+# Parse the CSV content using StringIO and csv.reader
+csv_reader = csv.reader(StringIO(csv_content))
+
+# Skip the header row if your CSV has one (optional)
+next(csv_reader, None)
+
+switch_dict = {
+            "string": case1,
+            "boolean": case2,
+            "number": case3
+        }
+
+fields = []
+for row in csv_reader:
+    name = row[0]
+    description = row[1]
+    data_type = row[3]
+    is_sensitive = row[2]
+    typeClass = switch_dict.get(data_type)()
+    field = SchemaFieldClass(
+                    fieldPath=name,
+                    type=SchemaFieldDataTypeClass(type=typeClass),
+                    nativeDataType=data_type,  # use this to provide the type of the field in the source system's vernacular
+                    description=description,
+                    lastModified=AuditStampClass(
+                        time=1640692800000, actor="urn:li:corpuser:ingestion"
+                    ),
+                )
+    fields.append(field)
+    
+event: MetadataChangeProposalWrapper = MetadataChangeProposalWrapper(
+    entityUrn=make_dataset_urn(platform="Denodo", name=dataset, env="PROD"),
+    aspect=SchemaMetadataClass(
+        schemaName="customer",  # not used
+        platform=make_data_platform_urn("Denodo"),  # important <- platform must be an urn
+        version=0,  # when the source system has a notion of versioning of schemas, insert this in, otherwise leave as 0
+        hash="",  # when the source system has a notion of unique schemas identified via hash, include a hash, else leave it as empty string
+        platformSchema=OtherSchemaClass(rawSchema="__insert raw schema here__"),
+        lastModified=AuditStampClass(
+            time=1640692800000, actor="urn:li:corpuser:ingestion"
+        ),
+        fields=fields,
+    ),
+)
+# Create rest emitter
+rest_emitter = DatahubRestEmitter(gms_server="http://datamgmtdemo01.eastasia.cloudapp.azure.com:31080", token=token)
+rest_emitter.emit(event)
